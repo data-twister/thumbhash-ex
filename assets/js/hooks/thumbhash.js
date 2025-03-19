dc47034ffc43a448ee08758cb680a778ff47ea08
mithereal <mithereal@gmail.com>
2025-03-19 15:12:47 -0700

    create installer

--- assets/js/hooks/thumbhash.js
+++ assets/js/hooks/thumbhash.js
@@ -1,4 +1,7 @@
 export default {
+const binaryToBase64 = binary => btoa(String.fromCharCode(...binary))
+const base64ToBinary = base64 => new Uint8Array(atob(base64).split('').map(x => x.charCodeAt(0)))
+
     mounted() {
       let { data } = this.el.dataset;
       let thumbHashFromBase64 = base64ToBinary(data.thumbhash)
@@ -6,7 +9,7 @@ export default {
 
       this.el.addEventListener("error", (ev) => {
         ev.preventDefault();
-        ev.target.src = url(${placeholderURL})
+        ev.target.src = url(placeholderURL)
         ev.onerror = null
       });
     }
